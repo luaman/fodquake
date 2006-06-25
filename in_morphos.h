960d7d33d003d253ec5c66d49e4ca2c32abc4c11
Mark Olsen <bigfoot@fodquake.net>
2006-06-25 03:32:51 +0000

    Added missing prototypes.

--- in_morphos.h
+++ in_morphos.h
@@ -2,3 +2,9 @@
 
 extern char keyconv[];
 extern struct EmulLibEntry myinputhandler;
+
+void *Sys_Input_Init(struct Screen *screen, struct Window *window);
+void Sys_Input_Shutdown(void *inputdata);
+void Sys_Input_GetEvents(void *inputdata);
+void Sys_Input_GetMouseMovement(void *inputdata, int *mousex, int *mousey);
+
