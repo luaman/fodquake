3d329366fdf831281633f35d93bbefd38d7a288c
Mark Olsen <bigfoot@fodquake.net>
2006-12-03 17:38:09 +0000

    sys/socket.h is required for the AF_INET define on FreeBSD

--- cl_main.c
+++ cl_main.c
@@ -47,6 +47,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef _WIN32
 #include <unistd.h>
 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
