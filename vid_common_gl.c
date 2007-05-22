ba3c47a14098062fcb71e47a8220623f9470eb9f
Mark Olsen <bigfoot@fodquake.net>
2007-05-22 23:46:04 +0000

    Disabled the ATI blacklisting.

--- vid_common_gl.c
+++ vid_common_gl.c
@@ -101,7 +101,7 @@ void CheckMultiTextureExtensions (void) {
 	glGetIntegerv(GL_MAX_TEXTURE_UNITS_ARB, &gl_textureunits);
 	gl_textureunits = min(gl_textureunits, 4);
 
-	if (COM_CheckParm("-maxtmu2") || !strcmp(gl_vendor, "ATI Technologies Inc."))
+	if (COM_CheckParm("-maxtmu2")/* || !strcmp(gl_vendor, "ATI Technologies Inc.")*/)
 		gl_textureunits = min(gl_textureunits, 2);
 
 	if (gl_textureunits < 2)
