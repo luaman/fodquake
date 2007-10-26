1c4d7f1205afffd5108a6ad99022dcca00489f91
Mark Olsen <bigfoot@fodquake.net>
2007-10-26 16:00:00 +0000

    Removed unused QUAKE_GAME define.

--- common.h
+++ common.h
@@ -35,8 +35,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #pragma warning( disable : 4244 4127 4201 4214 4514 4305 4115 4018)
 #endif
 
-#define	QUAKE_GAME			// as opposed to utilities
-
 typedef unsigned char 		byte;
 
 typedef enum {false, true} qboolean;
