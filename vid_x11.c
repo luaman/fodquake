d8958d686955729a0a6d8adccee9671eb979ada9
Mark Olsen <bigfoot@fodquake.net>
2008-03-24 19:23:52 +0000

    Added Sys_Video_GetWidth() and Sys_Video_GetHeight().

--- vid_x11.c
+++ vid_x11.c
@@ -764,6 +764,24 @@ void Sys_Video_SetWindowTitle(void *display, const char *text)
 	XStoreName(d->x_disp, d->x_win, text);
 }
 
+unsigned int Sys_Video_GetWidth(void *display)
+{
+	struct display *d;
+
+	d = display;
+
+	return d->width;
+}
+
+unsigned int Sys_Video_GetHeight(void *display)
+{
+	struct display *d;
+
+	d = display;
+
+	return d->height;
+}
+
 unsigned int Sys_Video_GetBytesPerRow(void *display)
 {
 	struct display *d;
