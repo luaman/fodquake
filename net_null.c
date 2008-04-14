379f0e17a7dc2eafd12ce7f00e90b46ee0cf096f
Mark Olsen <bigfoot@fodquake.net>
2008-04-14 04:34:31 +0000

    Network implementation for those with no networking.

---
+++ net_null.c
@@ -0,0 +1,42 @@
+#include "quakedef.h"
+#include "net.h"
+#include "sys_net.h"
+
+struct SysNetData *Sys_Net_Init()
+{
+	return 0;
+}
+
+void Sys_Net_Shutdown(struct SysNetData *netdata)
+{
+}
+
+qboolean Sys_Net_ResolveName(struct SysNetData *netdata, const char *name, struct netaddr *address)
+{
+	return false;
+}
+
+struct SysSocket *Sys_Net_CreateSocket(struct SysNetData *netdata, enum netaddrtype addrtype)
+{
+	return 0;
+}
+
+void Sys_Net_DeleteSocket(struct SysNetData *netdata, struct SysSocket *socket)
+{
+}
+
+qboolean Sys_Net_Bind(struct SysNetData *netdata, struct SysSocket *socket, unsigned short port)
+{
+	return false;
+}
+
+int Sys_Net_Send(struct SysNetData *netdata, struct SysSocket *socket, const void *data, int datalen, const struct netaddr *address)
+{
+	return -1;
+}
+
+int Sys_Net_Receive(struct SysNetData *netdata, struct SysSocket *socket, void *data, int datalen, struct netaddr *address)
+{
+	return -1;
+}
+
