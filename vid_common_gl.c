57cd7a354f81867a0b357af216d75e42d06f424e
Mark Olsen <bigfoot@fodquake.net>
2009-06-26 09:15:02 +0000

    Reindented.

--- vid_common_gl.c
+++ vid_common_gl.c
@@ -65,38 +65,44 @@ byte color_black[4] = {0, 0, 0, 0};
 
 qboolean OnChange_gl_ext_texture_compression(cvar_t *, char *);
 
-cvar_t	gl_strings = {"gl_strings", "", CVAR_ROM};
-cvar_t	gl_ext_texture_compression = {"gl_ext_texture_compression", "0", 0, OnChange_gl_ext_texture_compression};
+cvar_t gl_strings = {"gl_strings", "", CVAR_ROM};
+cvar_t gl_ext_texture_compression = {"gl_ext_texture_compression", "0", 0, OnChange_gl_ext_texture_compression};
 
 /************************************* EXTENSIONS *************************************/
 
-qboolean CheckExtension (const char *extension) {
+qboolean CheckExtension (const char *extension)
+{
 	const char *start;
 	char *where, *terminator;
 
 	if (!gl_extensions && !(gl_extensions = glGetString (GL_EXTENSIONS)))
 		return false;
 
-	
 	if (!extension || *extension == 0 || strchr (extension, ' '))
 		return false;
 
-	for (start = gl_extensions; (where = strstr(start, extension)); start = terminator) {
+	for (start = gl_extensions; (where = strstr(start, extension)); start = terminator)
+	{
 		terminator = where + strlen (extension);
 		if ((where == start || *(where - 1) == ' ') && (*terminator == 0 || *terminator == ' '))
 			return true;
 	}
+
 	return false;
 }
 
-void CheckMultiTextureExtensions (void) {
-	if (!COM_CheckParm("-nomtex") && CheckExtension("GL_ARB_multitexture")) {
+void CheckMultiTextureExtensions (void)
+{
+	if (!COM_CheckParm("-nomtex") && CheckExtension("GL_ARB_multitexture"))
+	{
 		if (strstr(gl_renderer, "Savage"))
 			return;
+
 		qglMultiTexCoord2f = (void *) qglGetProcAddress("glMultiTexCoord2fARB");
 		qglActiveTexture = (void *) qglGetProcAddress("glActiveTextureARB");
 		if (!qglMultiTexCoord2f || !qglActiveTexture)
 			return;
+
 		Com_Printf ("Multitexture extensions found\n");
 		gl_mtexable = true;
 	}
@@ -116,24 +122,26 @@ void CheckMultiTextureExtensions (void) {
 		Com_Printf("Enabled %i texture units on hardware\n", gl_textureunits);
 }
 
-void GL_CheckExtensions (void) {
+void GL_CheckExtensions (void)
+{
 	CheckMultiTextureExtensions ();
 
 	gl_combine = CheckExtension("GL_ARB_texture_env_combine");
 		gl_add_ext = CheckExtension("GL_ARB_texture_env_add");
 
-	if (CheckExtension("GL_ARB_texture_compression")) {
+	if (CheckExtension("GL_ARB_texture_compression"))
+	{
 		Com_Printf("Texture compression extensions found\n");
 	}
 }
 
-qboolean OnChange_gl_ext_texture_compression(cvar_t *var, char *string) {
+qboolean OnChange_gl_ext_texture_compression(cvar_t *var, char *string)
+{
 	float newval = Q_atof(string);
 
 	if (!newval == !var->value)
 		return false;
 
-	
 	gl_alpha_format = newval ? GL_COMPRESSED_RGBA_ARB : GL_RGBA;
 	gl_solid_format = newval ? GL_COMPRESSED_RGB_ARB : GL_RGB;
 
@@ -150,7 +158,8 @@ void GL_CvarInit()
 	Cvar_ResetCurrentGroup();
 }
 
-void GL_Init (void) {
+void GL_Init (void)
+{
 	gl_vendor = glGetString (GL_VENDOR);
 	Com_Printf ("GL_VENDOR: %s\n", gl_vendor);
 	gl_renderer = glGetString (GL_RENDERER);
@@ -188,7 +197,8 @@ void GL_Init (void) {
 
 /************************************* VID GAMMA *************************************/
 
-void Check_Gamma (unsigned char *pal) {
+void Check_Gamma (unsigned char *pal)
+{
 	float inf;
 	unsigned char palette[768];
 	int i;
@@ -200,14 +210,19 @@ void Check_Gamma (unsigned char *pal) {
 
 	Cvar_SetDefault (&v_gamma, vid_gamma);
 
-	if (vid_gamma != 1){
-		for (i = 0; i < 256; i++){
+	if (vid_gamma != 1)
+	{
+		for (i = 0; i < 256; i++)
+		{
 			inf = 255 * pow((i + 0.5) / 255.5, vid_gamma) + 0.5;
 			if (inf > 255)
 				inf = 255;
+
 			vid_gamma_table[i] = inf;
 		}
-	} else {
+	}
+	else
+	{
 		for (i = 0; i < 256; i++)
 			vid_gamma_table[i] = i;
 	}
@@ -220,7 +235,8 @@ void Check_Gamma (unsigned char *pal) {
 
 /************************************* HW GAMMA *************************************/
 
-void VID_SetPalette (unsigned char *palette) {
+void VID_SetPalette (unsigned char *palette)
+{
 	int i;
 	byte *pal;
 	unsigned r,g,b, v, *table;
@@ -228,7 +244,8 @@ void VID_SetPalette (unsigned char *palette) {
 	// 8 8 8 encoding
 	pal = palette;
 	table = d_8to24table;
-	for (i = 0; i < 256; i++) {
+	for (i = 0; i < 256; i++)
+	{
 		r = pal[0];
 		g = pal[1];
 		b = pal[2];
@@ -243,7 +260,8 @@ void VID_SetPalette (unsigned char *palette) {
 	pal = palette;
 	table = d_8to24table2;
 
-	for (i = 0; i < 256; i++) {
+	for (i = 0; i < 256; i++)
+	{
 		r = pal[0] * (2.0 / 1.5); if (r > 255) r = 255;
 		g = pal[1] * (2.0 / 1.5); if (g > 255) g = 255;
 		b = pal[2] * (2.0 / 1.5); if (b > 255) b = 255;
@@ -251,5 +269,7 @@ void VID_SetPalette (unsigned char *palette) {
 		v = (255 << 24) + (b << 16) + (g << 8) + (r << 0);
 		*table++ = LittleLong(v);
 	}
+
 	d_8to24table2[255] = 0;	// 255 is transparent
 }
+
