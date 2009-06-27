290c638dc67b94fe1b191fdda73f88eb34bffc9c
Mark Olsen <bigfoot@fodquake.net>
2009-06-27 14:08:36 +0000

    And the includes I forgot.

--- host.c
+++ host.c
@@ -22,6 +22,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <stdarg.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "quakedef.h"
 #include "pmove.h"
