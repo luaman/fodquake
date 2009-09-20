986f71852fa4368b94c7723efd06017c0623b512
Mark Olsen <bigfoot@fodquake.net>
2009-09-20 08:50:14 +0000

    There is not Movie_Init().

--- movie.h
+++ movie.h
@@ -23,7 +23,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define __MOVIE_H_
 
 void Movie_CvarInit(void);
-void Movie_Init(void);
 double Movie_StartFrame(void);
 void Movie_FinishFrame(void);
 qboolean Movie_IsCapturing(void);
