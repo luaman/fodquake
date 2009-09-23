1f9886274010ea5774eba1c26c0daa0f5b83b0c5
Mark Olsen <bigfoot@fodquake.net>
2009-09-23 13:33:13 +0000

    vid_menudrawfn, vid_menukeyfn and VID_SetMode are dead.

--- vid.h
+++ vid.h
@@ -54,8 +54,6 @@ typedef struct
 extern	viddef_t	vid;				// global video state
 extern	unsigned short	d_8to16table[256];
 extern	unsigned	d_8to24table[256];
-extern void (*vid_menudrawfn)(void);
-extern void (*vid_menukeyfn)(int key);
 
 void	VID_SetPalette (unsigned char *palette);
 // called at startup and after any gamma correction
@@ -74,10 +72,6 @@ void VID_Shutdown (void);
 
 void VID_Update (vrect_t *rects);
 // flushes the given rectangles from the view buffer to the screen
-
-int VID_SetMode (int modenum, unsigned char *palette);
-// sets the mode; only used by the Quake engine for resetting to mode 0 (the
-// base mode) on memory allocation failures
 
 int VID_GetKeyEvent(keynum_t *key, qboolean *down);
 
