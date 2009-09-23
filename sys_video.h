0b7dab64e7461985a7191ffb61a91bd7f53bb96a
Mark Olsen <bigfoot@fodquake.net>
2009-09-23 08:40:35 +0000

    OK, do include qtypes.h

--- sys_video.h
+++ sys_video.h
@@ -17,6 +17,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#include "qtypes.h"
+
 void Sys_Video_CvarInit(void);
 void *Sys_Video_Open(const char *mode, unsigned int width, unsigned int height, int fullscreen, unsigned char *palette);
 void Sys_Video_Close(void *display);
