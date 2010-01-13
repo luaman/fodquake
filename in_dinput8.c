ec7256be910595e8d423021436a827be9b90c214
Mark Olsen <bigfoot@fodquake.net>
2010-01-13 15:41:09 +0000

    Use sys_win.h instead of winquake.h

--- in_dinput8.c
+++ in_dinput8.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 #include "input.h"
 #include "keys.h"
-#include "winquake2.h"
+#include "sys_win.h"
 #include "in_dinput8.h"
 
 #define DINPUTNUMEVENTS 32
