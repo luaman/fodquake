af3373d178a6e92288ab4295039976472197c9b1
Mark Olsen <bigfoot@fodquake.net>
2010-01-16 21:12:53 +0000

    Build PNG support into the win32 version.

--- config.h
+++ config.h
@@ -1,10 +1,8 @@
-#ifndef _WIN32
 #define USE_ZLIB 1
 #define USE_PNG 1
-#ifdef __MORPHOS__
+#if defined(__MORPHOS__) || defined(_WIN32)
 #define USE_JPEG 0
 #else
 #define USE_JPEG 1
-#endif
 #endif
 
