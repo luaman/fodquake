e621fbb0a41169ca3cc551598168577e3f7bbb34
Mark Olsen <bigfoot@fodquake.net>
2010-06-13 06:52:03 +0000

    Ah yes... Don't define ED_NewString().

--- progs.h
+++ progs.h
@@ -75,9 +75,6 @@ void PR_Profile_f (void);
 edict_t *ED_Alloc (void);
 void ED_Free (edict_t *ed);
 
-char	*ED_NewString (char *string);
-// returns a copy of the string allocated from the server's string heap
-
 void ED_Print (edict_t *ed);
 void ED_Write (FILE *f, edict_t *ed);
 char *ED_ParseEdict (char *data, edict_t *ent);
