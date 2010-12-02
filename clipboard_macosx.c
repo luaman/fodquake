258e9d08d65093432beec382caf45c80280f2e24
Mark Olsen <bigfoot@fodquake.net>
2010-12-02 19:38:37 +0000

    Some dummy files

---
+++ clipboard_macosx.c
@@ -0,0 +1,14 @@
+
+const char *Sys_Video_GetClipboardText(void *display)
+{
+	return 0;
+}
+
+void Sys_Video_FreeClipboardText(void *display, const char *text)
+{
+}
+
+void Sys_Video_SetClipboardText(void *display, const char *text)
+{
+}
+
---
+++ vid_mode_macosx.c
@@ -0,0 +1,19 @@
+
+const char * const *Sys_Video_GetModeList(void)
+{
+	return 0;
+}
+
+void Sys_Video_FreeModeList(const char * const *displaymodes)
+{
+}
+
+const char *Sys_Video_GetModeDescription(const char *mode)
+{
+	return 0;
+}
+
+void Sys_Video_FreeModeDescription(const char *modedescription)
+{
+}
+
