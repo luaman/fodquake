6d08c77794d0d6537e8161ad99926a585bc277c8
Mark Olsen <bigfoot@fodquake.net>
2011-03-26 22:12:27 +0000

    Zapped some implicit function declaration warnings.

--- cl_screen.c
+++ cl_screen.c
@@ -29,6 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "keys.h"
 #include "image.h"
 #include "menu.h"
+#include "netqw.h"
 #include "sbar.h"
 
 #include "teamplay.h"
