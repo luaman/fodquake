6bccd59aa31cf4834f0046918ab59509d89e5b87
Mark Olsen <bigfoot@fodquake.net>
2011-10-11 17:50:01 +0000

    image.c: Don't use Con_Print() directly.

--- image.c
+++ image.c
@@ -735,7 +735,7 @@ static qboolean PNG_LoadLibrary(void)
 	}
 
 	fprintf(stderr, "Unable to open libpng - PNG image loading and saving will be disabled\n");
-	Con_Print("Unable to open libpng - PNG image loading and saving will be disabled\n");
+	Com_Printf("Unable to open libpng - PNG image loading and saving will be disabled\n");
 
 	return false;
 }
@@ -1364,7 +1364,7 @@ static qboolean JPEG_LoadLibrary(void)
 	}
 
 	fprintf(stderr, "Unable to open libpng - PNG image loading and saving will be disabled\n");
-	Con_Print("Unable to open libpng - PNG image loading and saving will be disabled\n");
+	Com_Printf("Unable to open libpng - PNG image loading and saving will be disabled\n");
 
 	return false;
 }
