1ffc01cad3ed553ae5c2af31a82a6e1c5855bcd6
Mark Olsen <bigfoot@fodquake.net>
2011-10-15 21:50:41 +0000

    vid_tinygl.c: Added the missing proto/graphics.h include.

--- vid_tinygl.c
+++ vid_tinygl.c
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <cybergraphx/cybergraphics.h>
 
 #include <proto/exec.h>
+#include <proto/graphics.h>
 #include <proto/intuition.h>
 #include <proto/cybergraphics.h>
 
