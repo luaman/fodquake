f1972342287c4b4b0163226a35da854d94791ac4
Mark Olsen <bigfoot@fodquake.net>
2011-10-16 07:00:39 +0000

    sys_win.c: Cosmetics.

--- sys_win.c
+++ sys_win.c
@@ -43,7 +43,7 @@ int do_stdin = 0;
 qboolean stdin_ready;
 
 
-static qboolean OnChange_sys_highpriority (cvar_t *, char *);
+static qboolean OnChange_sys_highpriority(cvar_t *, char *);
 static cvar_t	sys_highpriority = {"sys_highpriority", "0", 0, OnChange_sys_highpriority};
 
 
@@ -53,14 +53,16 @@ static HANDLE	qwclsemaphore;
 static HANDLE	tevent;
 static HANDLE	hinput, houtput;
 
-void MaskExceptions (void);
-void Sys_PopFPCW (void);
-void Sys_PushFPCW_SetHigh (void);
+void MaskExceptions(void);
+void Sys_PopFPCW(void);
+void Sys_PushFPCW_SetHigh(void);
 
-int Sys_SetPriority(int priority) {
-    DWORD p;
+int Sys_SetPriority(int priority)
+{
+	DWORD p;
 
-	switch (priority) {
+	switch (priority)
+	{
 		case 0:	p = IDLE_PRIORITY_CLASS; break;
 		case 1:	p = NORMAL_PRIORITY_CLASS; break;
 		case 2:	p = HIGH_PRIORITY_CLASS; break;
@@ -71,24 +73,31 @@ int Sys_SetPriority(int priority) {
 	return SetPriorityClass(GetCurrentProcess(), p);
 }
 
-static qboolean OnChange_sys_highpriority (cvar_t *var, char *s) {
+static qboolean OnChange_sys_highpriority(cvar_t *var, char *s)
+{
 	int ok, q_priority;
 	char *desc;
 	float priority;
 
 	priority = Q_atof(s);
-	if (priority == 1) {
+	if (priority == 1)
+	{
 		q_priority = 2;
 		desc = "high";
-	} else if (priority == -1) {
+	}
+	else if (priority == -1)
+	{
 		q_priority = 0;
 		desc = "low";
-	} else {
+	}
+	else
+	{
 		q_priority = 1;
 		desc = "normal";
 	}
 
-	if (!(ok = Sys_SetPriority(q_priority))) {
+	if (!(ok = Sys_SetPriority(q_priority)))
+	{
 		Com_Printf("Changing process priority failed\n");
 		return true;
 	}
@@ -104,10 +113,12 @@ FILE IO
 ===============================================================================
 */
 
-int	Sys_FileTime (char *path) {
+int Sys_FileTime(char *path)
+{
 	FILE *f;
-	
-	if ((f = fopen(path, "rb"))) {
+
+	if ((f = fopen(path, "rb")))
+	{
 		fclose(f);
 		return 1;
 	}
@@ -121,33 +132,36 @@ SYSTEM IO
 ===============================================================================
 */
 
-void Sys_MakeCodeWriteable (unsigned long startaddr, unsigned long length) {
+void Sys_MakeCodeWriteable(unsigned long startaddr, unsigned long length)
+{
 	DWORD  flOldProtect;
 
 	//@@@ copy on write or just read-write?
 	if (!VirtualProtect((LPVOID)startaddr, length, PAGE_READWRITE, &flOldProtect))
-   		Sys_Error("Protection change failed");
+		Sys_Error("Protection change failed");
 }
 
-void Sys_Error (char *error, ...) {
+void Sys_Error(char *error, ...)
+{
 	va_list argptr;
 	char text[1024];
 
-	Host_Shutdown ();
+	Host_Shutdown();
 
-	va_start (argptr, error);
-	vsnprintf (text, sizeof(text), error, argptr);
-	va_end (argptr);
+	va_start(argptr, error);
+	vsnprintf(text, sizeof(text), error, argptr);
+	va_end(argptr);
 
 	MessageBox(NULL, text, "Error", 0 /* MB_OK */ );
 
 	if (qwclsemaphore)
-		CloseHandle (qwclsemaphore);
+		CloseHandle(qwclsemaphore);
 
-	exit (1);
+	exit(1);
 }
 
-void Sys_Printf (char *fmt, ...) {
+void Sys_Printf(char *fmt, ...)
+{
 	va_list argptr;
 	char text[1024];
 	DWORD dummy;
@@ -161,22 +175,23 @@ void Sys_Printf (char *fmt, ...) {
 	if (!dedicated)
 		return;
 
-	va_start (argptr,fmt);
-	vsnprintf (text, sizeof(text), fmt, argptr);
-	va_end (argptr);
+	va_start(argptr,fmt);
+	vsnprintf(text, sizeof(text), fmt, argptr);
+	va_end(argptr);
 
-	WriteFile (houtput, text, strlen(text), &dummy, NULL);
+	WriteFile(houtput, text, strlen(text), &dummy, NULL);
 }
 
-void Sys_Quit (void) {
+void Sys_Quit(void)
+{
 	if (tevent)
-		CloseHandle (tevent);
+		CloseHandle(tevent);
 
 	if (qwclsemaphore)
-		CloseHandle (qwclsemaphore);
+		CloseHandle(qwclsemaphore);
 
 	if (dedicated)
-		FreeConsole ();
+		FreeConsole();
 
 	exit (0);
 }
@@ -218,7 +233,7 @@ double Sys_DoubleTime(void)
 
 	if (hwtimer)
 	{
-		QueryPerformanceCounter ((LARGE_INTEGER *)&pcount);
+		QueryPerformanceCounter((LARGE_INTEGER *)&pcount);
 		// TODO: check for wrapping
 		return (pcount - startcount) / pfreq;
 	}
@@ -247,33 +262,39 @@ unsigned long long Sys_IntTime()
 	return Sys_DoubleTime()*1000000;
 }
 
-char *Sys_ConsoleInput (void) {
-	static char	text[256];
+char *Sys_ConsoleInput(void)
+{
+	static char text[256];
 	static int len;
 	INPUT_RECORD rec;
 	int i, dummy, ch, numread, numevents;
 	char *textCopied;
 
-	while (1) {
-		if (!GetNumberOfConsoleInputEvents (hinput, &numevents))
-			Sys_Error ("Error getting # of console events");
+	while (1)
+	{
+		if (!GetNumberOfConsoleInputEvents(hinput, &numevents))
+			Sys_Error("Error getting # of console events");
 
 		if (numevents <= 0)
 			break;
 
 		if (!ReadConsoleInput(hinput, &rec, 1, &numread))
-			Sys_Error ("Error reading console input");
+			Sys_Error("Error reading console input");
 
 		if (numread != 1)
-			Sys_Error ("Couldn't read console input");
+			Sys_Error("Couldn't read console input");
 
-		if (rec.EventType == KEY_EVENT) {
-			if (rec.Event.KeyEvent.bKeyDown) {
+		if (rec.EventType == KEY_EVENT)
+		{
+			if (rec.Event.KeyEvent.bKeyDown)
+			{
 				ch = rec.Event.KeyEvent.uChar.AsciiChar;
-				switch (ch) {
+				switch (ch)
+				{
 					case '\r':
 						WriteFile(houtput, "\r\n", 2, &dummy, NULL);
-						if (len) {
+						if (len)
+						{
 							text[len] = 0;
 							len = 0;
 							return text;
@@ -288,14 +309,17 @@ char *Sys_ConsoleInput (void) {
 
 					default:
 						if ((ch == ('V' & 31)) /* ctrl-v */ ||
-							((rec.Event.KeyEvent.dwControlKeyState & SHIFT_PRESSED) && (rec.Event.KeyEvent.wVirtualKeyCode == VK_INSERT))) {
+							((rec.Event.KeyEvent.dwControlKeyState & SHIFT_PRESSED) && (rec.Event.KeyEvent.wVirtualKeyCode == VK_INSERT)))
+							{
 
 #if 0
-								if ((textCopied = Sys_GetClipboardData())) {
+								if ((textCopied = Sys_GetClipboardData()))
+								{
 									i = strlen(textCopied);
 									if (i + len >= sizeof(text))
 										i = sizeof(text) - len - 1;
-									if (i > 0) {
+									if (i > 0)
+									{
 										textCopied[i] = 0;
 										text[len] = 0;
 										strcat(text, textCopied);
@@ -304,8 +328,10 @@ char *Sys_ConsoleInput (void) {
 									}
 								}
 #endif
-							} else if (ch >= ' ') {
-								WriteFile(houtput, &ch, 1, &dummy, NULL);	
+							}
+							else if (ch >= ' ')
+							{
+								WriteFile(houtput, &ch, 1, &dummy, NULL);
 								text[len] = ch;
 								len = (len + 1) & 0xff;
 							}
@@ -318,21 +344,24 @@ char *Sys_ConsoleInput (void) {
 	return NULL;
 }
 
-BOOL WINAPI HandlerRoutine (DWORD dwCtrlType) {
-	switch (dwCtrlType) {
-		case CTRL_C_EVENT:		
+BOOL WINAPI HandlerRoutine(DWORD dwCtrlType)
+{
+	switch (dwCtrlType)
+	{
+		case CTRL_C_EVENT:
 		case CTRL_BREAK_EVENT:
 		case CTRL_CLOSE_EVENT:
 		case CTRL_LOGOFF_EVENT:
 		case CTRL_SHUTDOWN_EVENT:
-			Cbuf_AddText ("quit\n");
+			Cbuf_AddText("quit\n");
 			return true;
 	}
 	return false;
 }
 
 //Quake calls this so the system can register variables before host_hunklevel is marked
-void Sys_CvarInit (void) {
+void Sys_CvarInit(void)
+{
 
 	Cvar_SetCurrentGroup(CVAR_GROUP_SYSTEM_SETTINGS);
 	Cvar_Register(&sys_highpriority);
@@ -340,11 +369,12 @@ void Sys_CvarInit (void) {
 	Cvar_ResetCurrentGroup();
 }
 
-void Sys_Init (void)
+void Sys_Init(void)
 {
 }
 
-void Sys_Init_ (void) {
+void Sys_Init_(void)
+{
 	OSVERSIONINFO vinfo;
 
 	// allocate a named semaphore on the client so the front end can tell if it is alive
@@ -355,8 +385,8 @@ void Sys_Init_ (void) {
 			0,			//owner
 			"qwcl");	//Semaphore name/
 		if (!qwclsemaphore)
-			Sys_Error ("QWCL is already running on this system");
-		CloseHandle (qwclsemaphore);
+			Sys_Error("QWCL is already running on this system");
+		CloseHandle(qwclsemaphore);
 
 		qwclsemaphore = CreateSemaphore(
 			NULL,		//Security attributes
@@ -366,19 +396,19 @@ void Sys_Init_ (void) {
 	}
 
 #if 0
-	MaskExceptions ();
-	Sys_SetFPCW ();
+	MaskExceptions();
+	Sys_SetFPCW();
 #endif
 
-	Sys_InitDoubleTime ();
+	Sys_InitDoubleTime();
 
 	vinfo.dwOSVersionInfoSize = sizeof(vinfo);
 
 	if (!GetVersionEx(&vinfo))
-		Sys_Error ("Couldn't get OS info");
+		Sys_Error("Couldn't get OS info");
 
 	if ((vinfo.dwMajorVersion < 5) || (vinfo.dwPlatformId == VER_PLATFORM_WIN32s))
-		Sys_Error ("FodQuake requires at least Windows 2000");
+		Sys_Error("FodQuake requires at least Windows 2000");
 }
 
 /********************************* CLIPBOARD *********************************/
@@ -387,7 +417,8 @@ void Sys_Init_ (void) {
 /* Deprecated interface */
 #define SYS_CLIPBOARD_SIZE		256
 
-char *Sys_GetClipboardData(void) {
+char *Sys_GetClipboardData(void)
+{
 	HANDLE th;
 	char *clipText, *s, *t;
 	static char clipboard[SYS_CLIPBOARD_SIZE];
@@ -395,12 +426,14 @@ char *Sys_GetClipboardData(void) {
 	if (!OpenClipboard(NULL))
 		return NULL;
 
-	if (!(th = GetClipboardData(CF_TEXT))) {
+	if (!(th = GetClipboardData(CF_TEXT)))
+	{
 		CloseClipboard();
 		return NULL;
 	}
 
-	if (!(clipText = GlobalLock(th))) {
+	if (!(clipText = GlobalLock(th)))
+	{
 		CloseClipboard();
 		return NULL;
 	}
@@ -419,24 +452,28 @@ char *Sys_GetClipboardData(void) {
 #endif
 
 // copies given text to clipboard
-void Sys_CopyToClipboard(char *text) {
+void Sys_CopyToClipboard(char *text)
+{
 	char *clipText;
 	HGLOBAL hglbCopy;
 
 	if (!OpenClipboard(NULL))
 		return;
 
-	if (!EmptyClipboard()) {
+	if (!EmptyClipboard())
+	{
 		CloseClipboard();
 		return;
 	}
 
-	if (!(hglbCopy = GlobalAlloc(GMEM_DDESHARE, strlen(text) + 1))) {
+	if (!(hglbCopy = GlobalAlloc(GMEM_DDESHARE, strlen(text) + 1)))
+	{
 		CloseClipboard();
 		return;
 	}
 
-	if (!(clipText = GlobalLock(hglbCopy))) {
+	if (!(clipText = GlobalLock(hglbCopy)))
+	{
 		CloseClipboard();
 		return;
 	}
@@ -494,48 +531,51 @@ static void SleepUntilInput(int time)
 
 HINSTANCE	global_hInstance;
 
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
 	double time, oldtime, newtime;
 
 	global_hInstance = hInstance;
 
-	ParseCommandLine (lpCmdLine);
+	ParseCommandLine(lpCmdLine);
 
 	// we need to check some parms before Host_Init is called
-	COM_InitArgv (argc, argv);
+	COM_InitArgv(argc, argv);
 
 #if !defined(CLIENTONLY)
-	dedicated = COM_CheckParm ("-dedicated");
+	dedicated = COM_CheckParm("-dedicated");
 #endif
 
-	if (dedicated) {
+	if (dedicated)
+	{
 		if (!AllocConsole())
-			Sys_Error ("Couldn't allocate dedicated server console");
-		SetConsoleCtrlHandler (HandlerRoutine, TRUE);
-		SetConsoleTitle ("fqds");
-		hinput = GetStdHandle (STD_INPUT_HANDLE);
-		houtput = GetStdHandle (STD_OUTPUT_HANDLE);
+			Sys_Error("Couldn't allocate dedicated server console");
+		SetConsoleCtrlHandler(HandlerRoutine, TRUE);
+		SetConsoleTitle("fqds");
+		hinput = GetStdHandle(STD_INPUT_HANDLE);
+		houtput = GetStdHandle(STD_OUTPUT_HANDLE);
 	}
 
-	tevent = CreateEvent (NULL, FALSE, FALSE, NULL);
+	tevent = CreateEvent(NULL, FALSE, FALSE, NULL);
 	if (!tevent)
-		Sys_Error ("Couldn't create event");
+		Sys_Error("Couldn't create event");
 
 	Sys_Init_();
 
-	Sys_Printf ("Host_Init\n");
-	Host_Init (argc, argv);
+	Sys_Printf("Host_Init\n");
+	Host_Init(argc, argv);
 
-	oldtime = Sys_DoubleTime ();
+	oldtime = Sys_DoubleTime();
 
 	/* main window message loop */
-	while (1) {
+	while (1)
+	{
 		if (dedicated)
 			NET_Sleep(1);
 
-		newtime = Sys_DoubleTime ();
+		newtime = Sys_DoubleTime();
 		time = newtime - oldtime;
-		Host_Frame (time);
+		Host_Frame(time);
 		oldtime = newtime;
 
 	}
