bd7017b476d85f73e5be4495e8b8d8e506a3c730
Mark Olsen <bigfoot@fodquake.net>
2011-10-16 05:16:39 +0000

    sys.h: Removed the Sys_mkdir() prototype.

--- sys.h
+++ sys.h
@@ -27,7 +27,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // file IO
 int	Sys_FileTime (char *path);
-void Sys_mkdir (char *path);
 
 // memory protection
 void Sys_MakeCodeWriteable (unsigned long startaddr, unsigned long length);
