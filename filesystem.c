28e496cb0e3a2e34f3286809d346dd14ced4b700
Mark Olsen <bigfoot@fodquake.net>
2011-11-05 21:13:22 +0000

    Removed the inclusion of unistd.h where it isn't needed.

--- filesystem.c
+++ filesystem.c
@@ -20,7 +20,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "common.h"
 #include "sys_io.h"
