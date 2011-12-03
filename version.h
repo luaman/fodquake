b035ee9b87fbda9704203476e7a6fb761c7b33a3
Mark Olsen <bigfoot@fodquake.net>
2011-12-03 14:30:42 +0000

    version.h: Really.

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 // version.h
 
 #ifdef __GNUC__
-#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ != 6) || (__GNUC__ == 4 && __GNUC_MINOR__ == 6 && __GNUC_PATCHLEVEL < 2) || (__GNUC__ == 3 && __GNUC_MINOR__ < 3) || (__GNUC__ == 3 && __GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ < 6) || (__GNUC__ == 3 && __GNUC_MINOR__ == 4 && __GNUC_PATCHLEVEL__ < 6)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ != 6) || (__GNUC__ == 4 && __GNUC_MINOR__ == 6 && __GNUC_PATCHLEVEL__ < 2) || (__GNUC__ == 3 && __GNUC_MINOR__ < 3) || (__GNUC__ == 3 && __GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ < 6) || (__GNUC__ == 3 && __GNUC_MINOR__ == 4 && __GNUC_PATCHLEVEL__ < 6)
 #define COMPILERVERSIONSTRINGAPPEND " (broken GCC)"
 #endif
 #endif
