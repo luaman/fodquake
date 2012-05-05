47aa527d561ad2b3002a431849bb17fb2923a85f
Mark Olsen <bigfoot@fodquake.net>
2012-05-05 23:27:48 +0000

    Fixing Win32 build, part 2.

--- vid_wgl.c
+++ vid_wgl.c
@@ -54,12 +54,18 @@ struct display
 /* Some lame hacks to support OpenGL 1.3 */
 
 static void (APIENTRY *glMultiTexCoord2f_win)(GLenum target, GLfloat s, GLfloat t);
+static void (APIENTRY *glDrawRangeElements_win)(GLenum mode, GLuint start, GLuint end, GLsizei count, GLenum type, const GLvoid *indices);
 static void (APIENTRY *glClientActiveTexture_win)(GLenum texture);
 static void (APIENTRY *glActiveTexture_win)(GLenum texture);
 
 void glMultiTexCoord2f(GLenum target, GLfloat s, GLfloat t)
 {
 	glMultiTexCoord2f_win(target, s, t);
+}
+
+void glDrawRangeElements(GLenum mode, GLuint start, GLuint end, GLsizei count, GLenum type, const GLvoid *indices)
+{
+	glDrawRangeElements_win(mode, start, end, count, type, indices);
 }
 
 void glClientActiveTexture(GLenum texture)
@@ -518,10 +524,11 @@ void *Sys_Video_Open(const char *mode, unsigned int width, unsigned int height, 
 						BOOL (APIENTRY *swapinterval)(int);
 
 						glMultiTexCoord2f_win = (void *)wglGetProcAddress("glMultiTexCoord2f");
+						glDrawRangeElements_win = (void *)wglGetProcAddress("glDrawRangeElements");
 						glClientActiveTexture_win = (void *)wglGetProcAddress("glClientActiveTexture");
 						glActiveTexture_win = (void *)wglGetProcAddress("glActiveTexture");
 
-						if (glMultiTexCoord2f_win && glActiveTexture_win)
+						if (glMultiTexCoord2f_win && glDrawRangeElements_win && glClientActiveTexture_win && glActiveTexture_win)
 						{
 							/* Vsync is broken on Windows, no point in ever enabling it. */
 							swapinterval = wglGetProcAddress("wglSwapIntervalEXT");
