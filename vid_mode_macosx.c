e07852c26a646df23045ec90ce41df6e1be94bee
Florian Zwoch <fzwoch@gmail.com>
2012-05-05 14:30:49 +0200

    osx: added copyright notice to some files where they were missing (needed?) osx: minor fix for the vid mode listing

--- vid_mode_macosx.c
+++ vid_mode_macosx.c
@@ -1,3 +1,23 @@
+/*
+ Copyright (C) 2011 Florian Zwoch
+ Copyright (C) 2011 Mark Olsen
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ 
+ See the GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <ApplicationServices/ApplicationServices.h>
@@ -37,7 +57,16 @@ const char * const *Sys_Video_GetModeList(void)
 		ret[i] = strdup(buf);
 		if (ret[i] == NULL)
 		{
-			return NULL; // may leak!
+			unsigned int j;
+			
+			for (j = 0; j < i; j++)
+			{
+				free(ret[j]);
+			}
+			
+			CFRelease(modes);
+			
+			return NULL;
 		}
 	}
 	
@@ -72,7 +101,7 @@ const char *Sys_Video_GetModeDescription(const char *mode)
 		return NULL;
 	}
 	
-	sprintf(buf, "%ux%u", width, height);
+	snprintf(buf, sizeof(buf), "%ux%u", width, height);
 	
 	ret = strdup(buf);
 	if (ret == NULL)
