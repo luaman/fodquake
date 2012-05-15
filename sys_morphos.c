14dc735488ecbbfeca20796d951b9422fd837f00
Mark Olsen <bigfoot@fodquake.net>
2012-05-15 16:19:44 +0000

    More AROS build fixing.

--- sys_morphos.c
+++ sys_morphos.c
@@ -18,7 +18,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include <exec/exec.h>
+#ifndef AROS
 #include <exec/system.h>
+#endif
 #include <dos/dos.h>
 
 #include <proto/exec.h>
