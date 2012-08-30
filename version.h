5d5f8c6206bbe96dd368333c891e74f69e14b61e
Mark Olsen <bigfoot@fodquake.net>
2012-08-30 12:44:18 +0000

    version.h: LLVM unfortunately advertises itself as a broken version of GCC. Add an explicit check for LLVM and avoid complaining about GCC then.

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 // version.h
 
-#ifdef __GNUC__
+#if defined(__GNUC__) && !defined(__llvm__)
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ != 6) || (__GNUC__ == 4 && __GNUC_MINOR__ == 6 && __GNUC_PATCHLEVEL__ < 2) || (__GNUC__ == 3 && __GNUC_MINOR__ < 3) || (__GNUC__ == 3 && __GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ < 6) || (__GNUC__ == 3 && __GNUC_MINOR__ == 4 && __GNUC_PATCHLEVEL__ < 6)
 #define COMPILERVERSIONSTRINGAPPEND " (broken GCC)"
 #endif
